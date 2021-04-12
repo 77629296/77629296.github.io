7d91615a6e958a4ba98cb9ce318488fd02401c03
leeson <77629296@qq.com>
2021-04-12 23:24:04 +0800

    feat: 修改标题/增加资料引用

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: 'blog',
+  title: '日常学习打卡',
   favicon: 'https://tva1.sinaimg.cn/large/008eGmZEly1gp40v7bpzhj300w00wa9t.jpg',
   logo: 'https://tva1.sinaimg.cn/large/008eGmZEly1gp40w64rcfj30b40b40t1.jpg',
   outputPath: 'docs-dist',
