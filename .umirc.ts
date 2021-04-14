fdeba1d2f7faa8d6fcc4191c1acde20e75b12f09
leeson <77629296@qq.com>
2021-04-14 22:15:09 +0800

    feat: 新增单例模式

--- .umirc.ts
+++ .umirc.ts
@@ -6,5 +6,4 @@ export default defineConfig({
   favicon: 'https://tva1.sinaimg.cn/large/008eGmZEly1gp40v7bpzhj300w00wa9t.jpg',
   logo: 'https://tva1.sinaimg.cn/large/008eGmZEly1gp40w64rcfj30b40b40t1.jpg',
   outputPath: 'docs-dist',
-  // more config: https://d.umijs.org/config
 });
