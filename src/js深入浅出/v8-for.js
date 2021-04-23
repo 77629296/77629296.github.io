3fa0f14080df2f437825270fafc33d44f0a76032
leeson <77629296@qq.com>
2021-04-23 08:16:45 +0800

    feat: 增加v8执行流程

---
+++ src/js深入浅出/v8-for.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+// for (var i = 0; i < 3; i++) {
+//   console.log(i)
+// }
---
+++ src/js深入浅出/v8-var.js
@@ -0,0 +1,9 @@
+var a = 2;
+{
+  var b = 3;
+}
+// let a = 2;
+// {
+//   let b = 3;
+// }
+// /Users/leeson/Documents/dev/v8-mac64-dbg-8.4.109/d8 --print-ast ast.js
